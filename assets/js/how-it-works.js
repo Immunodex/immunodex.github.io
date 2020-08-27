23ab88c81c7c6e1fb1ebaeb5e8870877fdc848c6
18prernasingh <18prernasingh@gmail.com>
2020-08-27 16:54:09 +0800

    Health declaration --> health screening

--- assets/js/how-it-works.js
+++ assets/js/how-it-works.js
@@ -1,10 +1,10 @@
 var firstInfoTitle = [
   "First, an employee books a doctor's appointment.",
-  "First, request a health declaration from your user.",
+  "First, request a health screening from your user.",
 ];
 var firstInfoDesc = [
   "An authorized employee books an appointment for a health assessment or diagnostic testing (as mandated by your organization or law) with a physician on Immunodex. Our app reminds them with location and time prior to the reservation.",
-  "An authorized user (staff, student or client) is notified to complete a health declaration prior to entering your premises. This notification can be set to automatically run at regular intervals.",
+  "An authorized user (staff, student or client) is notified to complete a health screening prior to entering your premises. This notification can be set to automatically run at regular intervals.",
 ];
 var firstImg = [
   "../images/immunodex-how-1-new-2.gif",
